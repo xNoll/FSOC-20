b667fbf3927f89e62885123a94bdfb69d6e07721
xNoll <el_alex_9@hotmail.com>
2020-06-27 14:28:04 +0200

    finished 1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,36 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = (props) => {
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const Part = (props) => {
   return (
     <div>
       <p>
-        Hello {props.name}, with {props.age} years old; this is a const attached
-        to the main <b>App</b> var
+        <b>Name</b> {props.part}, <b>exercises:</b> {props.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part
+        part={props.course.parts[0].name}
+        exercises={props.course.parts[0].exercises}
+      />
+      <Part
+        part={props.course.parts[1].name}
+        exercises={props.course.parts[1].exercises}
+      />
+      <Part
+        part={props.course.parts[2].name}
+        exercises={props.course.parts[2].exercises}
+      />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
       </p>
     </div>
   );
 };
 
 const App = () => {
-  console.log("Hello from component");
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  const link = <a href="https://github.com/xNoll/FSOC-20">xNoll's repo</a>;
-  const name = "Const";
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
-    <>
-      <h1>Hello world, it is, {now.toString()}</h1>
-      <p>{link}</p>
-      <Hello name="Visitor" age="32" />
-      <Hello name={name} age={a + b} />
-    </>
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
   );
 };
 
-ReactDOM.render(
-  React.createElement(App, null),
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
