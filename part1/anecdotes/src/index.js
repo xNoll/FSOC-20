aff26b71783cb8ff75d2a729ab6c39161e106636
xNoll <el_alex_9@hotmail.com>
2020-06-29 18:46:40 +0200

    finished 1-14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,36 +1,61 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
-const Button = (props) => <button onClick={props.handleClick}>Generate</button>;
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 const App = (props) => {
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
+  const highestVote = points.indexOf(Math.max(...points));
 
-  const handleClick = () => {
-    const randomAnecdote = Math.round(Math.random() * (anecdotes.length-1));
-    setSelected(randomAnecdote);
+  const handleVote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  }
+
+  const handleNextAnecdote = () => {
+    const randomAnecdoteSelection = Math.round(Math.random() * (anecdotes.length - 1));
+    setSelected(randomAnecdoteSelection);
   }
 
   return (
     <>
-      <h1>QUOTE OF THE DAY</h1>
+      <div>
+        <h1>QUOTE OF THE DAY</h1>
+      </div>
+      <hr />
       <div>
         {props.anecdotes[selected]}
       </div>
       <div>
-        <Button handleClick={handleClick} />
+        <em>- {points[selected]} votes</em>
+      </div>
+      <div>
+        <button onClick={handleVote}>Vote</button>{' '}
+        <button onClick={handleNextAnecdote}>Next</button>
+      </div>
+      <div>
+        <h2>Quote with the most votes</h2>
+      </div>
+      <div>
+        {props.anecdotes[highestVote]}
+      </div>
+      <div>
+        <em>- {points[highestVote]} votes</em>
       </div>
     </>
-  );
-};
+  )
+}
 
 const anecdotes = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
 
-ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+);
