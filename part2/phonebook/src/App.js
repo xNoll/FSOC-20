ed876199034e1e4be41a52cb76b3903d0f922153
xNoll <el_alex_9@hotmail.com>
2020-08-05 00:10:12 +0200

    finished 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import Person from './components/Person';
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const name = {
+      name: newName
+    }
+    if(persons.find(element => element.name === name.name)){
+      alert(`${name.name} is already on the list`)
+    } else {
+      setPersons(persons.concat(name))
+      setNewName('')
+    }
+    
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          <input
+            value = {newName}
+            onChange={handleNameChange} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        <ul>
+        {persons.map(person => 
+          <Person name={person.name} key={person.name} />
+        )}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Person = (prop) =>{
+
+    return (
+        <div>
+          <p>
+              {prop.name}
+          </p>
+        </div>
+      );
+}
+
+export default Person;
